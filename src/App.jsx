4900d6f114818c16a5f56a9d543e0a0b758b399a
Joao Melo <134986966+johnmelodev@users.noreply.github.com>
2025-02-28 22:36:10 +0000

    React JSX function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-const title = 'React';
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
     <div>
-      <h1>{title}</h1>
+      <h1>Hello {getTitle('React')}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
