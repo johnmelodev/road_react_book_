a95c9e1337aba8ad3a91c3629d66419e4745b33c
Joao Melo <134986966+johnmelodev@users.noreply.github.com>
2025-03-06 15:18:59 +0000

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import * as React from "react";
-import { useState } from "react";
+import {useState, useEffect} from 'react';
 
 const App = () => {
   const stories = [
     {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
       num_comments: 3,
       points: 4,
       objectID: 0,
     },
     {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
       num_comments: 2,
       points: 5,
       objectID: 1,
     },
   ];
 
-  const [searchTerm, setSearchTerm] = useState("React");
+  const [searchTerm, setSearchTerm] = useState(
+    // define que o local storage usando a notacao search vai ser a primeira opcao. caso nao tenha nada salvo vai iniciar como 'React'
+    localStorage.getItem('search') || 'React'
+  );
+
+  useEffect(() => {
+    // 'search'chave de notacao que guarda o valor searchTerm
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm]);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const searchedStories = stories.filter(({ title }) =>
-    title.toLowerCase().includes(searchTerm.toLowerCase())
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
@@ -47,26 +54,31 @@ const App = () => {
 const Search = ({ search, onSearch }) => (
   <div>
     <label htmlFor="search">Search: </label>
-    <input id="search" type="text" value={search} onChange={onSearch} />
+    <input
+      id="search"
+      type="text"
+      value={search}
+      onChange={onSearch}
+    />
   </div>
 );
 
 const List = ({ list }) => (
   <ul>
-    {list.map((objectID, ...item) => (
-      <Item key={objectID} {...item}/>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} />
     ))}
   </ul>
 );
 
-const Item = ({ title, url, author, num_comments, points }) => (
+const Item = ({ item }) => (
   <li>
     <span>
-      <a href={url}>{title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{author}</span>
-    <span>{num_comments}</span>
-    <span>{points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </li>
 );
 
